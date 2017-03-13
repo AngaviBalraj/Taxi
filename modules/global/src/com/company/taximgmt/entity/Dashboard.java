0c8199907c9bf786a66ba862a6868f5da222bcab
Angavi Balraj <angavi.balraj@gmail.com>
2017-03-13 15:48:33 +0530

    Image

--- modules/global/src/com/company/taximgmt/entity/Dashboard.java
+++ modules/global/src/com/company/taximgmt/entity/Dashboard.java
@@ -3,14 +3,10 @@ package com.company.taximgmt.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import com.haulmont.cuba.core.entity.StandardEntity;
-import javax.persistence.InheritanceType;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.Inheritance;
-import javax.persistence.DiscriminatorColumn;
 
 @Table(name = "TAXIMGMT_DASHBOARD")
 @Entity(name = "taximgmt$Dashboard")
 public class Dashboard extends StandardEntity {
-    private static final long serialVersionUID = 1768150914388415835L;
+    private static final long serialVersionUID = 6429241133930771603L;
 
 }
--- modules/gui/src/com/company/taximgmt/gui/dashboard/DashboardEdit.java
+++ modules/gui/src/com/company/taximgmt/gui/dashboard/DashboardEdit.java
@@ -1,4 +1,4 @@
-package com.company.taximgmt.web.dashboard;
+package com.company.taximgmt.gui.dashboard;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.taximgmt.entity.Dashboard;
